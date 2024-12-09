56512180f4d85b2fe424327d25f04dc60a834f66
limboden <imbodenlu@gmail.com>
2024-12-09 00:07:15 -0500

    hard coded what the games would look like in the main page. wrote where some of that data might come from in comments, might add some more comments tomorrow

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,9 @@
 import '../style/Home.css'; // Ensure correct import path
+import { Link } from 'react-router-dom';
+// these arrays of images would come from the database and from the api. 
+const startImgArray = ["https://m.media-amazon.com/images/M/MV5BNDRhZjkyNWItMjkzOS00ZjUwLTk4NDctODY2ZjZkZGE4OGY2XkEyXkFqcGc@._V1_.jpg", "https://howlongtobeat.com/games/5203_League_of_Legends.jpg"];
+const loadingImgArray = ["https://www.lego.com/cdn/cs/set/assets/blt0f703dfbce999d88/Video-games-Juno-Carousel-card.jpg?fit=crop&format=jpg&quality=80&width=635&height=440&dpr=1"];
+const playedImgArray = ["https://image.api.playstation.com/vulcan/ap/rnd/202110/2000/aGhopp3MHppi7kooGE2Dtt8C.png"];
 
 const Home = () => {
 
@@ -8,19 +13,55 @@ const Home = () => {
         <div className="column-wrapper">
           <h2>Press Start</h2>
           <div className="column not-yet-played">
-            <p>*here a game pulled from the api will be displayed*</p> {/* Placeholder */}
+            {startImgArray &&
+              startImgArray.map((img) => (
+                <div className="card mb-3">
+                  {/* Create a link to this thought's page to view its comments using `<Link>` component */}
+                  <img src={img} width={'100%'} alt="" />
+                  <Link
+                    className="btn btn-primary btn-block btn-squared"
+                    to={`/thoughts/6754f48c45fb31fb1e09facd`}
+                  >
+                    Join the discussion on this game.
+                  </Link>
+                </div>
+              ))}
           </div>
         </div>
         <div className="column-wrapper">
           <h2>Loading</h2>
           <div className="column playing">
-            <p>*here a game pulled from the api will be displayed*</p> {/* Placeholder */}
+            {loadingImgArray &&
+              loadingImgArray.map((img) => (
+                <div className="card mb-3">
+                  {/* Create a link to this thought's page to view its comments using `<Link>` component */}
+                  <img src={img} width={'100%'} alt="" />
+                  <Link
+                    className="btn btn-primary btn-block btn-squared"
+                    to={`/thoughts/6754f48c45fb31fb1e09facd`}
+                  >
+                    Join the discussion on this game.
+                  </Link>
+                </div>
+              ))}
           </div>
         </div>
         <div className="column-wrapper">
           <h2>Well Played</h2>
           <div className="column played">
-            <p>*here a game pulled from the api will be displayed*</p> {/* Placeholder */}
+            {playedImgArray &&
+              playedImgArray.map((img) => (
+                <div className="card mb-3">
+                  {/* Create a link to this thought's page to view its comments using `<Link>` component */}
+                  <img src={img} width={'100%'} alt="" />
+                  <Link
+                    className="btn btn-primary btn-block btn-squared"
+                    to={`/thoughts/6754f48c45fb31fb1e09facd`}
+                  >
+                    Join the discussion on this game.
+                  </Link>
+                </div>
+              ))}
           </div>
         </div>
       </div>
