cfcb32a9b4ac3ce5e280662fbec2d503b3deaeb7
Micheleflare <michele.ferrario1515@gmail.com>
2024-12-12 12:32:46 -0500

    curr

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure the build output is directed to 'dist'
+    outDir: 'dist' 
   },
   server: {
     port: 3000,
